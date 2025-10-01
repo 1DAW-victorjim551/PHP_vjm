29592978718dbf01d811b2be3345de566b45c35d
unknown <2DAW-vjimmar0508@IESMARTINEZM.ES>
2025-10-01 14:58:15 +0200

    Subida 01/10/2025 insitutto

--- dwes_php_exmaple1-main/index.php
+++ dwes_php_exmaple1-main/index.php
@@ -1,88 +1,127 @@
 <?php
 
-/* Inicialización del entorno */
+/***** Inicialización del entorno ******/
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
 /* Zona de declaración de funciones */
-//Funciones de debugueo
+//*******Funciones de debugueo****
 function dump($var){
     echo '<pre>'.print_r($var,1).'</pre>';
 }
 
-//Función lógica presentación
-function getTableroMarkup($tableroData){
-    return '<div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile agua"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile tierra"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile agua"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile tierra"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile agua"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile tierra"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile agua"></div>
-        <div class="tile fuego"></div>
-        <div class="tile fuego"></div>
-        <div class="tile tierra"></div>';
+//*******Función lógica presentación**********+
+function getTableroMarkup ($tablero, $posPersonaje){
+    $output = '';
+    foreach ($tablero as $filaIndex => $datosFila) {
+        foreach ($datosFila as $columnaIndex => $tileType) {
+            if(isset($posPersonaje)&&($filaIndex == $posPersonaje['row'])&&($columnaIndex == $posPersonaje['col'])){
+                $output .= '<div class = "tile ' . $tileType . '"><img src="./src/super_musculitos.png"></div>';    
+            }else{
+                $output .= '<div class = "tile ' . $tileType . '"></div>';
+            }
+        }
+    }
+    return $output;
 }
+//******+Función Lógica de negocio************
+//El tablero es un array bidimensional en el que cada fila contiene 12 palabras cuyos valores pueden ser:
+// agua
+//fuego
+//tierra
+// hierba
+function leerArchivoCSV($rutaArchivoCSV) {
+    $tablero = [];
+
+    if (($puntero = fopen($rutaArchivoCSV, "r")) !== FALSE) {
+        while (($datosFila = fgetcsv($puntero)) !== FALSE) {
+            $tablero[] = $datosFila;
+        }
+        fclose($puntero);
+    }
+
+    return $tablero;
+}
+function leerInput(){
+    
+    $col = filter_input(INPUT_GET, 'col', FILTER_VALIDATE_INT);
+    $row = filter_input(INPUT_GET, 'row', FILTER_VALIDATE_INT);
+    $fila = $_GET['fila'] ?? 0;
+    $columna = $_GET['columna'] ?? 0;
+    return array('col' => $col,
+      'row' => $row
+);
+    
+//    return (isset($col) && isset($row))? array(
+//            'row' => $row,
+//            'col' => $col
+//        ) : null;    
+}
+//*****Lógica de negocio***********
+//Extracción de las variables de la petición
 
 
-//Lógica de negocio
-$tablero = [
+$posPersonaje = leerInput();
 
-];
+dump('$posPersonaje');
+dump($posPersonaje);
+$tablero = leerArchivoCSV('./data/tablero1.csv');
 
-//Lógica de presentación
-$tableroMarkup = getTableroMArkup($tablero);
+
+
+//*****+++Lógica de presentación*******
+$tableroMarkup = getTableroMarkup($tablero, $posPersonaje);
 
 
 ?>
-
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <!-- Minified version -->
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <title>Document</title>
     <style>
-        .contenedorTablero{
-            width:604px;
-            height: 604px;
-            border-radius: 5px;
+        .contenedorTablero {
+            width:600px;
+            height:600px;
             border: solid 2px grey;
             box-shadow: grey;
+            display:grid;
+            grid-template-columns: repeat(12, 1fr);
+            grid-template-rows: repeat(12, 1fr);
         }
-        .tile{
-            width: 50px;
-            height: 50px;
-            float:left;
-            margin:0;
-            padding:0;
-            border-width:0;
+        .tile {
+            float: left;
+            margin: 0;
+            padding: 0;
+            border-width: 0;
+            background-image: url("./src/464.jpg");
+            background-size: 209px;
+            background-repeat: none;
+            overflow: hidden;
         }
-        .fuego{
-            background-color:red;
+        .tile img{
+            max-width:100%;
         }
-        .tierra{
-            background-color:brown;
+        .fuego {
+            background-color: red;
+            background-position: -105px -52px;
         }
-        .agua{
-            background-color:blue;
+        .tierra {
+            background-color: brown;
+            background-position: -157px 0px;
+        }
+        .agua {
+            background-color: blue;
+            background-position: -53px 0px;
+        }
+        .hierba {
+            background-color: green;
+            background-position: 0px 0px;
         }
     </style>
 </head>
