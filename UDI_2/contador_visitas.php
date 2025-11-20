fb3ba986010a6d7351a8c1b8bd84ecea74557e1a
unknown <2DAW-vjimmar0508@IESMARTINEZM.ES>
2025-11-20 12:12:09 +0100

    Subida 20/11/2025

--- UDI_2/contador_visitas.php
+++ UDI_2/contador_visitas.php
@@ -5,12 +5,17 @@
     // }
     // $_SESSION['visitas']++;
     // echo "Número de visitas en esta sesión: " . $_SESSION['visitas'];
-
+     
     if (isset($_COOKIE['visita'])) {
-        $visita = $_COOKIE['visita'] + 1;
+        $visita = $_COOKIE['visita'];
+        if($_COOKIE['visita'] == 10){
+            setcookie('visita', $visita, time() + (-1));
+        }else {
+            $visita = $_COOKIE['visita'] + 1;
         setcookie('visita', $visita, time() + 3600);
         echo "Visita numero : " . "<b>$visita</b>";
-
+        }
+        
     }else {
         $visita = 1;
         setcookie('visita', $visita, time() + 3600);
